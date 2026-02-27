2f599ba4c1b3593520c5f148544719775d497680
Max van Asten <maxvasten@gmail.com>
2026-02-27 15:59:35 +0100

    Rewrote lexer tests to match parser tests format

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,73 +1,227 @@
 package lexer_test
 
 import (
-	"github.com/maxvanasten/gscp/lexer"
 	"testing"
+
+	"github.com/maxvanasten/gscp/lexer"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestLexer(t *testing.T) {
-	// =================
-	input := []byte("#include path\\to\\file;\ninit(arg1, arg2) {\n\tname = \"Max\";\n\tage = 23.5;\n\tprint(\"Hello \" + name + \". You are \" + age + \" years old.\");\n}")
-
-	targets := []lexer.Token{
-		{lexer.SYMBOL, "#include"},
-		{lexer.SYMBOL, "path\\to\\file"},
-		{lexer.TERMINATOR, ";"},
-		{lexer.NEWLINE, ""},
-		{lexer.SYMBOL, "init"},
-		{lexer.OPEN_PAREN, "("},
-		{lexer.SYMBOL, "arg1"},
-		{lexer.COMMA, ","},
-		{lexer.SYMBOL, "arg2"},
-		{lexer.CLOSE_PAREN, ")"},
-		{lexer.OPEN_CURLY, "{"},
-		{lexer.NEWLINE, ""},
-
-		{lexer.SYMBOL, "name"},
-		{lexer.ASSIGNMENT, "="},
-		{lexer.STRING, "Max"},
-		{lexer.TERMINATOR, ";"},
-		{lexer.NEWLINE, ""},
-
-		{lexer.SYMBOL, "age"},
-		{lexer.ASSIGNMENT, "="},
-		{lexer.NUMBER, "23.5"},
-		{lexer.TERMINATOR, ";"},
-		{lexer.NEWLINE, ""},
-
-		{lexer.SYMBOL, "print"},
-		{lexer.OPEN_PAREN, "("},
-		{lexer.STRING, "Hello "},
-		{lexer.OPERATOR, "+"},
-		{lexer.SYMBOL, "name"},
-		{lexer.OPERATOR, "+"},
-		{lexer.STRING, ". You are "},
-		{lexer.OPERATOR, "+"},
-		{lexer.SYMBOL, "age"},
-		{lexer.OPERATOR, "+"},
-		{lexer.STRING, " years old."},
-		{lexer.CLOSE_PAREN, ")"},
-		{lexer.TERMINATOR, ";"},
-		{lexer.NEWLINE, ""},
-
-		{lexer.CLOSE_CURLY, "}"},
-	}
-	// =================
+func assertTokens(t *testing.T, input []byte, targets []lexer.Token) {
+	t.Helper()
 
 	l := lexer.NewLexer(input)
-
 	tokens := l.GetTokens()
-	//for i, token := range tokens {
-	//	t.Logf("[%v] Type: %v, Content: %v\n", i, token.Type.ToString(), token.Content)
-	//}
 
-	if len(targets) != len(tokens) {
-		t.Fatalf("len(targets)(%v) != len(tokens)(%v)\n", len(targets), len(tokens))
+	assert.Equal(t, targets, tokens)
+}
+
+func TestLexerSymbol(t *testing.T) {
+	input := []byte("alpha")
+
+	targets := []lexer.Token{
+		{lexer.SYMBOL, "alpha"},
 	}
 
-	for i := range targets {
-		if targets[i] != tokens[i] {
-			t.Fatalf("targets[%v][%v](%v) != tokens[%v][%v](%v)\n", i, targets[i].Type.ToString(), targets[i].Content, i, tokens[i].Type.ToString(), tokens[i].Content)
-		}
+	assertTokens(t, input, targets)
+}
+
+func TestLexerNumber(t *testing.T) {
+	input := []byte("23.5")
+
+	targets := []lexer.Token{
+		{lexer.NUMBER, "23.5"},
 	}
+
+	assertTokens(t, input, targets)
+}
+
+func TestLexerString(t *testing.T) {
+	input := []byte("\"hello\"")
+
+	targets := []lexer.Token{
+		{lexer.STRING, "hello"},
+	}
+
+	assertTokens(t, input, targets)
+}
+
+func TestLexerTerminator(t *testing.T) {
+	input := []byte(";")
+
+	targets := []lexer.Token{
+		{lexer.TERMINATOR, ";"},
+	}
+
+	assertTokens(t, input, targets)
+}
+
+func TestLexerComma(t *testing.T) {
+	input := []byte(",")
+
+	targets := []lexer.Token{
+		{lexer.COMMA, ","},
+	}
+
+	assertTokens(t, input, targets)
+}
+
+func TestLexerNewline(t *testing.T) {
+	input := []byte("\n")
+
+	targets := []lexer.Token{
+		{lexer.NEWLINE, ""},
+	}
+
+	assertTokens(t, input, targets)
+}
+
+func TestLexerOpenParen(t *testing.T) {
+	input := []byte("(")
+
+	targets := []lexer.Token{
+		{lexer.OPEN_PAREN, "("},
+	}
+
+	assertTokens(t, input, targets)
+}
+
+func TestLexerCloseParen(t *testing.T) {
+	input := []byte(")")
+
+	targets := []lexer.Token{
+		{lexer.CLOSE_PAREN, ")"},
+	}
+
+	assertTokens(t, input, targets)
+}
+
+func TestLexerOpenBracket(t *testing.T) {
+	input := []byte("[")
+
+	targets := []lexer.Token{
+		{lexer.OPEN_BRACKET, "["},
+	}
+
+	assertTokens(t, input, targets)
+}
+
+func TestLexerCloseBracket(t *testing.T) {
+	input := []byte("]")
+
+	targets := []lexer.Token{
+		{lexer.CLOSE_BRACKET, "]"},
+	}
+
+	assertTokens(t, input, targets)
+}
+
+func TestLexerOpenCurly(t *testing.T) {
+	input := []byte("{")
+
+	targets := []lexer.Token{
+		{lexer.OPEN_CURLY, "{"},
+	}
+
+	assertTokens(t, input, targets)
+}
+
+func TestLexerCloseCurly(t *testing.T) {
+	input := []byte("}")
+
+	targets := []lexer.Token{
+		{lexer.CLOSE_CURLY, "}"},
+	}
+
+	assertTokens(t, input, targets)
+}
+
+func TestLexerAssignment(t *testing.T) {
+	input := []byte("=")
+
+	targets := []lexer.Token{
+		{lexer.ASSIGNMENT, "="},
+	}
+
+	assertTokens(t, input, targets)
+}
+
+func TestLexerCompoundAssignment(t *testing.T) {
+	input := []byte("a += 1; a -= 1; a *= 1; a /= 1;")
+
+	targets := []lexer.Token{
+		{lexer.SYMBOL, "a"},
+		{lexer.ASSIGNMENT, "+="},
+		{lexer.NUMBER, "1"},
+		{lexer.TERMINATOR, ";"},
+		{lexer.SYMBOL, "a"},
+		{lexer.ASSIGNMENT, "-="},
+		{lexer.NUMBER, "1"},
+		{lexer.TERMINATOR, ";"},
+		{lexer.SYMBOL, "a"},
+		{lexer.ASSIGNMENT, "*="},
+		{lexer.NUMBER, "1"},
+		{lexer.TERMINATOR, ";"},
+		{lexer.SYMBOL, "a"},
+		{lexer.ASSIGNMENT, "/="},
+		{lexer.NUMBER, "1"},
+		{lexer.TERMINATOR, ";"},
+	}
+
+	assertTokens(t, input, targets)
+}
+
+func TestLexerArithmeticOperators(t *testing.T) {
+	input := []byte("a + b - c * d / e;")
+
+	targets := []lexer.Token{
+		{lexer.SYMBOL, "a"},
+		{lexer.OPERATOR, "+"},
+		{lexer.SYMBOL, "b"},
+		{lexer.OPERATOR, "-"},
+		{lexer.SYMBOL, "c"},
+		{lexer.OPERATOR, "*"},
+		{lexer.SYMBOL, "d"},
+		{lexer.OPERATOR, "/"},
+		{lexer.SYMBOL, "e"},
+		{lexer.TERMINATOR, ";"},
+	}
+
+	assertTokens(t, input, targets)
+}
+
+func TestLexerComparisonOperators(t *testing.T) {
+	input := []byte("a < b; a > b; a <= b; a >= b; a == b; a != b; !a;")
+
+	targets := []lexer.Token{
+		{lexer.SYMBOL, "a"},
+		{lexer.OPERATOR, "<"},
+		{lexer.SYMBOL, "b"},
+		{lexer.TERMINATOR, ";"},
+		{lexer.SYMBOL, "a"},
+		{lexer.OPERATOR, ">"},
+		{lexer.SYMBOL, "b"},
+		{lexer.TERMINATOR, ";"},
+		{lexer.SYMBOL, "a"},
+		{lexer.OPERATOR, "<="},
+		{lexer.SYMBOL, "b"},
+		{lexer.TERMINATOR, ";"},
+		{lexer.SYMBOL, "a"},
+		{lexer.OPERATOR, ">="},
+		{lexer.SYMBOL, "b"},
+		{lexer.TERMINATOR, ";"},
+		{lexer.SYMBOL, "a"},
+		{lexer.OPERATOR, "=="},
+		{lexer.SYMBOL, "b"},
+		{lexer.TERMINATOR, ";"},
+		{lexer.SYMBOL, "a"},
+		{lexer.OPERATOR, "!="},
+		{lexer.SYMBOL, "b"},
+		{lexer.TERMINATOR, ";"},
+		{lexer.OPERATOR, "!"},
+		{lexer.SYMBOL, "a"},
+		{lexer.TERMINATOR, ";"},
+	}
+
+	assertTokens(t, input, targets)
 }
