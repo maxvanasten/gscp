543c037de2934595dce37d3a4a9d34b11d34df2a
Max van Asten <maxvasten@gmail.com>
2026-02-27 16:00:33 +0100

    Parse For loops

--- parser/parser.go
+++ parser/parser.go
@@ -110,6 +110,62 @@ func Parse(tokens []l.Token) ([]Node, []d.Diagnostic) {
 			}
 
 			arg_tokens := l.TokensUntilAny(tokens[index+1:], []l.TokenType{l.CLOSE_PAREN})
+			if len(output)-1 >= 0 {
+				previous_node := output[len(output)-1]
+				if previous_node.Type == "variable_reference" && previous_node.Data.VarName == "for" {
+					header_tokens := arg_tokens
+					if len(header_tokens) > 0 && header_tokens[len(header_tokens)-1].Type == l.CLOSE_PAREN {
+						header_tokens = header_tokens[:len(header_tokens)-1]
+					}
+
+					segments := [][]l.Token{}
+					buf := []l.Token{}
+					depth := 0
+					for _, ht := range header_tokens {
+						switch ht.Type {
+						case l.OPEN_PAREN:
+							depth++
+							buf = append(buf, ht)
+						case l.CLOSE_PAREN:
+							if depth > 0 {
+								depth--
+							}
+							buf = append(buf, ht)
+						case l.TERMINATOR:
+							if depth == 0 {
+								segments = append(segments, buf)
+								buf = []l.Token{}
+							} else {
+								buf = append(buf, ht)
+							}
+						default:
+							buf = append(buf, ht)
+						}
+					}
+					segments = append(segments, buf)
+					for len(segments) < 3 {
+						segments = append(segments, []l.Token{})
+					}
+
+					init_children, init_diags := Parse(segments[0])
+					cond_children, cond_diags := Parse(segments[1])
+					post_children, post_diags := Parse(segments[2])
+					for_header := Node{"for_header", NodeData{}, []Node{
+						{"for_init", NodeData{}, init_children},
+						{"for_condition", NodeData{}, cond_children},
+						{"for_post", NodeData{}, post_children},
+					}}
+
+					diagnostics = append(diagnostics, init_diags...)
+					diagnostics = append(diagnostics, cond_diags...)
+					diagnostics = append(diagnostics, post_diags...)
+
+					output = output[:len(output)-1]
+					output = append(output, for_header)
+					index += len(arg_tokens)
+					break
+				}
+			}
 
 			arg_token_slices := [][]l.Token{}
 			// Split arg_tokens into slices at top-level commas only
@@ -175,26 +231,36 @@ func Parse(tokens []l.Token) ([]Node, []d.Diagnostic) {
 
 			index += len(arg_tokens)
 		case l.OPEN_CURLY:
-			// Check if previous node is a function_call
 			if len(output)-1 < 0 {
 				break
 			}
 			previous_node := output[len(output)-1]
-			if previous_node.Type != "function_call" {
+			switch previous_node.Type {
+			case "function_call":
+				output = output[:len(output)-1]
+				// Get all tokens from OPEN_CURLY until CLOSE_CURLY
+				scope_tokens := l.TokensUntilAny(tokens[index+1:], []l.TokenType{l.CLOSE_CURLY})
+				// Parse those tokens into scope node
+				arg_node := Node{"args", NodeData{}, previous_node.Children}
+				scope_children, diags := Parse(scope_tokens)
+				scope_node := Node{"scope", NodeData{}, scope_children}
+				// Add function declararation node
+				output = append(output, Node{"function_declaration", NodeData{FunctionName: previous_node.Data.FunctionName}, []Node{arg_node, scope_node}})
+				diagnostics = append(diagnostics, diags...)
+				index += len(scope_tokens)
+			case "for_header":
+				output = output[:len(output)-1]
+				scope_tokens := l.TokensUntilAny(tokens[index+1:], []l.TokenType{l.CLOSE_CURLY})
+				scope_children, diags := Parse(scope_tokens)
+				scope_node := Node{"scope", NodeData{}, scope_children}
+				for_children := append(previous_node.Children, scope_node)
+				output = append(output, Node{"for_loop", NodeData{}, for_children})
+				diagnostics = append(diagnostics, diags...)
+				index += len(scope_tokens)
+			default:
 				output = append(output, Node{"open_curly", NodeData{}, []Node{}})
 				break
 			}
-			output = output[:len(output)-1]
-			// Get all tokens from OPEN_CURLY until CLOSE_CURLY
-			scope_tokens := l.TokensUntilAny(tokens[index+1:], []l.TokenType{l.CLOSE_CURLY})
-			// Parse those tokens into scope node
-			arg_node := Node{"args", NodeData{}, previous_node.Children}
-			scope_children, diags := Parse(scope_tokens)
-			scope_node := Node{"scope", NodeData{}, scope_children}
-			// Add function declararation node
-			output = append(output, Node{"function_declaration", NodeData{FunctionName: previous_node.Data.FunctionName}, []Node{arg_node, scope_node}})
-			diagnostics = append(diagnostics, diags...)
-			index += len(scope_tokens)
 		default:
 		}
 		index++
