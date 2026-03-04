abaa75aeda3faa3d54d0cd9698048e61355f9306
Max van Asten <maxvasten@gmail.com>
2026-03-04 14:30:33 +0100

    changed usage, now accepts stdin or a filepath

--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
+	"io"
 	"os"
-	"strings"
 
 	"github.com/maxvanasten/gscp/diagnostics"
-	"github.com/maxvanasten/gscp/generator"
 	"github.com/maxvanasten/gscp/lexer"
 	"github.com/maxvanasten/gscp/parser"
 )
@@ -20,52 +18,39 @@ type ParseOutput struct {
 }
 
 func main() {
-	parsePath := flag.String("p", "", "Parse GSC file into AST")
-	generatePath := flag.String("g", "", "Generate GSC from AST JSON")
-	flag.Parse()
-	if (*parsePath == "" && *generatePath == "") || (*parsePath != "" && *generatePath != "") {
-		fmt.Fprintln(os.Stderr, "Usage: gscp -p input_file.gsc OR gscp -g input_ast.json")
+	args := os.Args[1:]
+	var data []byte
+	var err error
+	if len(args) == 0 {
+		// Get input from stdin
+		data, err = io.ReadAll(os.Stdin)
+	} else if len(args) == 1 {
+		// Get input from file
+		data, err = os.ReadFile(args[0])
+	} else {
+		// Incorrect usage
+		fmt.Fprintln(os.Stderr, "Usage: gscp input_file.gsc OR echo \"some_var = 20;\" | gscp")
 		os.Exit(1)
 	}
 
-	if *parsePath != "" {
-		data, err := os.ReadFile(*parsePath)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
-			os.Exit(1)
-		}
-
-		l := lexer.NewLexer(data)
-		tokens := l.GetTokens()
-		lexerDiagnostics := l.GetDiagnostics()
-
-		ast, parseDiagnostics := parser.Parse(tokens)
-		output := ParseOutput{
-			AST:         ast,
-			Tokens:      tokens,
-			Diagnostics: append(lexerDiagnostics, parseDiagnostics...),
-		}
-		if err := json.NewEncoder(os.Stdout).Encode(output); err != nil {
-			fmt.Fprintf(os.Stderr, "Error encoding json: %v\n", err)
-			os.Exit(1)
-		}
-		return
-	}
-
-	data, err := os.ReadFile(*generatePath)
+	// Handle data
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
-		os.Exit(1)
-	}
-	var ast []parser.Node
-	if err := json.Unmarshal(data, &ast); err != nil {
-		fmt.Fprintf(os.Stderr, "Error decoding json: %v\n", err)
+		fmt.Fprintf(os.Stderr, "Error getting input: %v\n", err)
 		os.Exit(1)
 	}
 
-	lines := []string{}
-	for _, node := range ast {
-		lines = append(lines, generator.Generate(node))
+	l := lexer.NewLexer(data)
+	tokens := l.GetTokens()
+	lexerDiagnostics := l.GetDiagnostics()
+
+	ast, parseDiagnostics := parser.Parse(tokens)
+	output := ParseOutput{
+		AST:         ast,
+		Tokens:      tokens,
+		Diagnostics: append(lexerDiagnostics, parseDiagnostics...),
 	}
-	fmt.Fprintln(os.Stdout, strings.Join(lines, "\n"))
+	if err := json.NewEncoder(os.Stdout).Encode(output); err != nil {
+		fmt.Fprintf(os.Stderr, "Error encoding json: %v\n", err)
+		os.Exit(1)
+	}
 }
